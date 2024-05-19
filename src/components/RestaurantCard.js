0d1239d218e124b729c8a150c83f9ccb28a2e312
sai charan <dsaicharan2002@gmail.com>
2024-05-19 23:02:40 +0530

    structure the app file system

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,21 @@
+import constants from "../utils/constants";
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, locality, cuisines, avgRatingString } =
+    resData?.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={constants.CDN_URL + cloudinaryImageId}
+      />
+
+      <h3>{name}</h3>
+      <h4>{locality}</h4>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRatingString} stars</h4>
+    </div>
+  );
+};
+export default RestaurantCard;
